2c54f3ec1fe8c68bb42cc2d302c58976d7566c2c
daven3302 <daven3302@gmail.com>
2021-07-04 04:06:36 +0800

    finish NI declaretion and definition

--- NI/network_interface.cpp
+++ NI/network_interface.cpp
@@ -11,6 +11,40 @@ Write_Addr::Write_Addr(Byte awid,Int awaddr,Byte awlen,Byte awsize,Byte awburst,
     AWPORT = awport;   
 }
 
+
+Write_Data::Write_Data(Byte wid,Int wdata,Byte wstrb,Byte wlast)
+{
+    WID = wid;
+    WDATA = wdata;
+    WSTRB = wstrb;
+    WLAST = wlast;
+}
+
+Write_Responce::Write_Responce(Byte bid,Byte bresp)
+{
+    BID = bid;
+    BRESP = bresp;
+}
+
+Read_Addr::Read_Addr(Byte arid,Int araddr,Byte arlen,Byte arsize,Byte arburst,Byte arlock,Byte arcache,Byte arport)
+{
+    ARID = arid;
+    ARADDR = araddr;
+    ARLEN = arlen;
+    ARSIZE = arsize;
+    ARLOCK = arlock;
+    ARCACHE = arcache;
+    ARPORT = arport;   
+}
+
+Read_Data::Read_Data(Byte rid,Int rdata,Byte rstrb,Byte rlast)
+{
+    RID = rid;
+    RDATA = rdata;
+    RSTRB = rstrb;
+    RLAST = rlast;
+}
+
 ostream& operator << (ostream& s, Write_Addr x)
 {
     s << "AWID\t=\t" << (int)x.AWID << endl;
@@ -19,5 +53,41 @@ ostream& operator << (ostream& s, Write_Addr x)
     s << "AWLOCK\t=\t" << (int)x.AWLOCK << endl;
     s << "AWCACHE\t=\t" << (int)x.AWCACHE << endl;
     s << "AWPROT\t=\t" << (int)x.AWPORT;
+    return s;
+}
+
+ostream& operator << (ostream& s, Write_Responce x)
+{
+    s << "BID\t=\t" << (int)x.BID << endl;
+    s << "BRESP\t=\t" << (int)x.BRESP;
+    return s;
+}
+
+ostream& operator << (ostream& s, Read_Data x)
+{
+    s << "RID\t=\t" << (int)x.RID << endl;
+    s << "RDATA\t=\t" << x.RDATA << endl;
+    s << "RSTRB\t=\t" << (int)x.RSTRB << endl;
+    s << "RLAST\t=\t" << (int)x.RLAST;
+    return s;
+}
+
+ostream& operator << (ostream& s, Read_Addr x)
+{
+    s << "ARID\t=\t" << (int)x.ARID << endl;
+    s << "ARADDR\t=\t" << x.ARADDR << endl;
+    s << "ARLEN\t=\t" << (int)x.ARLEN << endl;
+    s << "ARLOCK\t=\t" << (int)x.ARLOCK << endl;
+    s << "ARCACHE\t=\t" << (int)x.ARCACHE << endl;
+    s << "ARPROT\t=\t" << (int)x.ARPORT;
+    return s;
+}
+
+ostream& operator << (ostream& s, Write_Data x)
+{
+    s << "WID\t=\t" << (int)x.WID << endl;
+    s << "WDATA\t=\t" << x.WDATA << endl;
+    s << "WSTRB\t=\t" << (int)x.WSTRB << endl;
+    s << "WLAST\t=\t" << (int)x.WLAST;
     return s;
 }
--- NI/network_interface.h
+++ NI/network_interface.h
@@ -27,18 +27,58 @@ class Write_Addr
 
 class Write_Data
 {
-    Byte WID : 4;
-    Int WDATA : 32;
-    Byte WSTRB : 4;
-    Byte WLAST : 1;
+    private:
+        Byte WID : 4;
+        Int WDATA : 32;
+        Byte WSTRB : 4;
+        Byte WLAST : 1;
+    public:
+        Write_Data(Byte wid = 0,Int wdata = 0,Byte wstrb = 0,Byte wlast = 0);
+        friend ostream& operator << (ostream&,Write_Data);
 };
 
 class Write_Responce
 {
     Byte BID : 4;
     Byte BRESP : 2;
+    Write_Responce(Byte bid = 0,Byte bresp = 0);
+    friend ostream& operator << (ostream&,Write_Responce);
+};
+
+class Read_Addr
+{
+    private:
+        //define signals
+        Byte ARID : 4;
+        Int ARADDR : 32;
+        Byte ARLEN : 4;
+        Byte ARSIZE : 3;
+        Byte ARBURST : 2;
+        Byte ARLOCK : 2;
+        Byte ARCACHE : 4;
+        Byte ARPORT : 3;
+    public:
+        //define constructors
+        Read_Addr(Byte arid = 0,Int araddr = 0,Byte arlen = 0,Byte arsize = 0,Byte arburst = 0,Byte arlock = 0,Byte arcache = 0,Byte arport = 0);
+        friend ostream& operator << (ostream&, Read_Addr);
+};
+
+class Read_Data
+{
+    private:
+        Byte RID : 4;
+        Int RDATA : 32;
+        Byte RSTRB : 4;
+        Byte RLAST : 1;
+    public:
+        Read_Data(Byte rid = 0,Int rdata = 0,Byte rstrb = 0,Byte rlast = 0);
+        friend ostream& operator << (ostream&,Read_Data);
 };
 
 ostream& operator << (ostream&, Write_Addr);
+ostream& operator << (ostream&, Write_Data);
+ostream& operator << (ostream&, Write_Responce);
+ostream& operator << (ostream&, Read_Addr);
+ostream& operator << (ostream&, Read_Data);
 
 #endif
